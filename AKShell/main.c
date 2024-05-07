e265bceecb25866fdf3cb45ec5219a182fdfb4b2
Haktex <167474591+Haktex@users.noreply.github.com>
2024-05-07 14:22:36 -0600

    Update main.c

--- AKShell/main.c
+++ AKShell/main.c
@@ -1,13 +1,3 @@
-#include <stdio.h> //printf(), fprintf(), stderr, getchar(), perror()
-#include <stdlib.h> //malloc(), //realloc(), free(), exit(), execvp(), EXIT_SUCCESS, EXIT_FAILURE
-
-void akSh_loop(void);
-
 int main(void) {
-    akSh_loop();
-    return EXIT_SUCCESS;
-}
-
-void akSh_loop(void) {
-
+    return 0;
 }
